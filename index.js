d8f87abb494f402ddc4f28995563731fc8795e64
ElenaDRFS <elenafullstack@gmail.com>
2023-09-24 17:36:34 +0200

    terminado

---
+++ index.js
@@ -0,0 +1,56 @@
+let variableSinValor;
+
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = PI * 2;
+let booleanoAnd = booleano1 && booleano2;
+let booleanoNot = !booleano1;
+let booleanoMix0 = (booleano1 || booleano2) && (!booleano1 || !booleano2);
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp ++;
+let incrementarAntes = 2;
+let resultadoAntes = ++ incrementarAntes;
+var contarHasta10_2 = 0;
+    for (var contarHasta10_2 = 0; contarHasta10_2 != 10 ; contarHasta10_2 ++){};
+
+var postI = 0;
+var postJ = 0;
+    for (let i=0; i<11; i++){
+        postI += postJ++;
+    }
+
+var sumaPares = 0;
+    for(let i = 0; i < 10; i++) {
+       if(i % 2 === 0){
+            sumaPares += i;
+       }
+    }
+
+let variableValorNumerico = 3;
+const MiNombre = 'elena';
+const MiNumeroFav = 6;
+var booleanoOr = booleano1 || booleano2;
+var booleanoMix1 = (booleano1 && TAU/2 ===PI) || (variableValorNumerico >= MiNumeroFav);
+
+var seisNoEsNueve = 6 !== 9;
+var booleanoMix2 = variableValorNumerico > 0 || -(MiNumeroFav * TAU);
+var valorSuma = MiNumeroFav + variableValorNumerico;
+var valorResta = MiNumeroFav - variableValorNumerico;
+var valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+var valorDivision = MiNumeroFav / 3;
+var contarHasta10 = 0;
+    while (contarHasta10 != 10){
+        contarHasta10++;
+    }
+var preI = 0;
+var preJ = 0;
+    for(let i = 0; i < 11; i++){
+        preI += ++preJ;
+    }
+var sumaImpares = 0;
+    for (let i = 0; i < 10; i++){
+        if (i % 2 !== 0){
+            sumaImpares += i;
+        }
+    }
